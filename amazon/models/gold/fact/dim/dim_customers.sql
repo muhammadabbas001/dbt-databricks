c45860f678fff1e62d65895d0e3bec7eea2308be
Muhammad Abbas <muhammdabbas.cs@gmail.com>
2026-07-17 22:30:44 +0500

    added dim_customers view

---
+++ amazon/models/gold/fact/dim/dim_customers.sql
@@ -0,0 +1,20 @@
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country,
+    ci.cst_marital_status AS marital_status,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr 
+        ELSE COALESCE(ca.gen, 'n/a')  			   
+    END AS gender,
+    ca.bdate AS birthdate,
+    ci.cst_create_date
+FROM
+    {{ ref('silver_crm_cust_info') }} AS ci
+LEFT JOIN {{ ref('silver_erp_cust_az12') }} AS ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN {{ ref('silver_erp_loc_a101') }} AS la
+    ON ci.cst_key = la.cid;
